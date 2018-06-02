7ce7e980361a9d055b56bf9e8415dca8390d482b
Szu Yung Wang <johnny123tw@gmail.com>
2018-06-03 04:04:39 +0900

    Add reflaction test for generic class

--- src/com/learnjavageneric/Main.java
+++ src/com/learnjavageneric/Main.java
@@ -3,6 +3,6 @@ package com.learnjavageneric;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Event.Suit.DIAMONDS);
+        //System.out.println(Event.Suit.DIAMONDS);
     }
 }
